558c53e2042c4659048a76d49929f38c5e1a0751
Dave O'Connor <doc+gitkraken@skynet.ie>
2019-11-03 17:42:16 -0800

    Cleanup of logging for data_helpers

--- sitemapper/data_helpers.py
+++ sitemapper/data_helpers.py
@@ -2,8 +2,6 @@ import logging
 import requests
 from io import BytesIO
 from lxml import etree
-
-logger = logging.getLogger(__name__)
 # use Bytes throughout because that's how lxml says XML should be used
 
 
@@ -11,16 +9,14 @@ def download_uri_data(uri):
     """
     returns file object
     """
-    logger.info('download_uri_data requesting: %s' % uri)
+    logger = logging.getLogger(__name__)
+    logger.info('Requesting data from: {}'.format(uri))
     # using requests to follow any redirects that happen
     headers = {'Content-Type': 'application/xml;charset=utf-8'}
     r = requests.get(uri, headers=headers)
     # ensure it's the decompressed content
     r.raw.decode_content = True
-    logger.debug("r.content: {}, type: {}".format(
-        r.content,
-        type(r.content)
-    ))
+    logger.debug("Request content: {}".format(r.content))
     return r.content
 
 
@@ -28,17 +24,15 @@ def data_to_element(data):
     """
     data parameter should be bytes
     """
-    logger.debug('dataToElement retrieved: %s ' % data)
+    logger = logging.getLogger(__name__)
     content = BytesIO(data)
-    logger.debug('dataToElement BytesIO: %s ' % content)
-
     root = None
     try:
         utf8_parser = etree.XMLParser(encoding='utf-8')
         downloaded_xml = etree.parse(content, parser=utf8_parser)
-        logger.debug("downloaded_xml {}".format(downloaded_xml))
+        logger.debug("Downloaded: {}".format(downloaded_xml))
         root = downloaded_xml.getroot()
-        logger.debug("download root {}".format(root))
+        logger.debug("Downloaded root {}".format(root))
     except SyntaxError as err:
         logger.warning("Parsing failed {}".format(err))
         raise SyntaxError from err
