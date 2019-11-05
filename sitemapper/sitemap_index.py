e679c0e1825a3cc73bf9e5e571d866ed5bc21bbb
Dave O'Connor <doc+gitkraken@skynet.ie>
2019-11-04 16:03:57 -0800

    Implementation of sitemap_index

--- sitemapper/sitemap_index.py
+++ sitemapper/sitemap_index.py
@@ -1,16 +1,44 @@
-from sitemapper.site_mapper_iterator import SiteMapperIterator
+from sitemapper.sitemap import Sitemap
 import logging
-logger = logging.getLogger(__name__)
 
 
-class SiteMapper:
-    def __init__(self, url):
-        self.wrapped = self.process_url(url)
+class SitemapIndex:
+    def __init__(self, index_element):
+        """
+        Creates a 'sitemaps' property, an iterator for children of an lxml <sitemapindex> representation
+        :param index_element: lxml 'sitemapindex' element
+        """
+        self.sitemaps = self.sitemaps_from_sitemap_index_element(index_element)
 
     @staticmethod
-    def process_url(url):
-        logger.debug("_process_url: {}".format(url))
-        return []
+    def sitemap_from_sitemap_element(sitemap_element):
+        """
+        Creates a Sitemap instance for each sitemap element passed
+        :param sitemap_element: lxml representation of a <sitemap> element
+        :return: Sitemap instance
+        """
+        logger = logging.getLogger(__name__)
+        sitemap_data = {}
+        for ele in sitemap_element:
+            name = ele.xpath('local-name()')
+            value = ele.xpath('text()')[0]
+            sitemap_data[name] = value
 
-    def __iter__(self):
-        return SiteMapperIterator(self.wrapped)
+        msg = "Returning sitemap object with data: {}"
+        logger.debug(msg.format(sitemap_data))
+        return Sitemap(**sitemap_data)
+
+    @staticmethod
+    def sitemaps_from_sitemap_index_element(index_element):
+        """
+        Iterator to return the sitemaps from a lxml <sitemapindex> element
+        :param index_element: lxml representation of a <sitemapindex> element
+        :return: iter(Sitemap) instances
+        """
+        logger = logging.getLogger(__name__)
+        msg = "Generating sitemaps from {}"
+        logger.debug(msg.format(index_element))
+        # handle child elements, <sitemap>
+        sitemaps = index_element.findall("./*")
+        for sm_element in sitemaps:
+            yield SitemapIndex.sitemap_from_sitemap_element(sm_element)
