7abe3fee77fbb267ec4b6cecc7862c46e270c140
Dave O'Connor <doc+git@skynet.ie>
2021-04-15 23:53:02 -0700

    Finish of upload script

--- pypi-build.sh
+++ pypi-build.sh
@@ -4,3 +4,10 @@ source venv/bin/activate
 python3 --version
 python3 -m pip install -r requirements.txt
 python3 -m pytest
+if [[ $? == 1 ]]; then
+	exit 1
+fi;
+python3 -m pip install twine
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload -u __token__ -p $TEST_PASSWORD --non-interactive --repository testpypi dist/*
+python3 -m twine upload -u __token__ -p $LIVE_PASSWORD --non-interactive dist/*
